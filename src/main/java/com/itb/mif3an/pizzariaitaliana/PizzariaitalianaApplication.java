936b825635019becf1a39c318a052daa1797c336
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2025-08-12 21:49:31 -0300

    Configuração da conexão com o banco de dados

--- src/main/java/com/itb/mif3an/pizzariaitaliana/PizzariaitalianaApplication.java
+++ src/main/java/com/itb/mif3an/pizzariaitaliana/PizzariaitalianaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PizzariaitalianaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PizzariaitalianaApplication.class, args);
+		System.out.println("Projeto em execução na porta 8080");
 	}
 
 }
